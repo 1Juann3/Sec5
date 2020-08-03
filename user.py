4b23f5984ba78adf64772b9aaa7c3cd65cc3235b
1Juann3 <1juannx3>
2020-08-03 10:12:20 -0500

    added /register

--- user.py
+++ user.py
@@ -1,6 +1,9 @@
 import sqlite3
+from flask_restful import Resource, reqparse
+
 
 class User:
+
     def __init__(self, _id, username, password):
         self.id = _id
         self.username = username
@@ -11,7 +14,7 @@ class User:
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
-        query ="SELECT * FROM users WHERE username=?"
+        query = "SELECT * FROM users WHERE username=?"
         result = cursor.execute(query, (username,))
         row = result.fetchone()
         if row:
@@ -20,15 +23,14 @@ class User:
             user = None
 
         connection.close()
-        return user 
-
+        return user
 
     @classmethod
     def find_by_id(cls, _id):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
-        query ="SELECT * FROM users WHERE id=?"
+        query = "SELECT * FROM users WHERE id=?"
         result = cursor.execute(query, (_id,))
         row = result.fetchone()
         if row:
@@ -37,4 +39,32 @@ class User:
             user = None
 
         connection.close()
-        return user 
+        return user
+
+
+class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    parser.add_argument('password',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+
+    def post(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        data = UserRegister.parser.parse_args()
+
+        query = "INSERT INTO users VALUES (NULL, ?, ?)"
+        cursor.execute(query, (data['username'], data['password']))
+
+        connection.commit()
+        connection.close
+
+        return {'message': 'User created successfully'}, 201
